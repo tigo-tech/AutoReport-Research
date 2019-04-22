7e996bb9a5064562c1afc91f39f5364a1856635c
zsj <15528377112@163.com>
2019-04-22 21:19:50 +0800

    add single module

---
+++ SQL Format/define.py
@@ -0,0 +1,34 @@
+select_str = "SELECT"
+from_str = "FROM"
+group_str = "GROUP BY"
+order_str = "ORDER BY"
+blank_str = " "
+comma_str = ","
+as_str = "AS"
+t_str = "t"
+left_str = "("
+right_str = ")"
+join_str = "JOIN"
+on_str = "ON"
+all_str = "*"
+equal_str = "="
+count_str = "COUNT"
+distin_str = "DISTINCT"
+sum_str = "SUM"
+avg_str = "AVG"
+where_str = "WHERE"
+count_num = "count_num"
+avg_num = "avg_num"
+sum_num = "sum_num"
+
+mode_dict = {
+    1: count_str,
+    2: avg_str,
+    3: sum_str
+}
+
+mode_name = {
+    1:count_num,
+    2:avg_num,
+    3:sum_str
+}
---
+++ SQL Format/single_module.py
@@ -0,0 +1,45 @@
+# single module
+from define import *
+
+
+class single_module:
+    def __init__(self, input):
+        self.__cols = input[:-1]
+        self.__table = input[-1]
+
+    def naive_aggregation(self, mode):
+        format_str = select_str
+        select_output = []
+        if len(self.__cols) == 1:
+            # format_str += LIKE " COUNT(columnName) AS count_num " OR AVG OR SUM OR others
+            format_str += blank_str + mode_dict[mode] + left_str + \
+                self.__cols[0] + right_str + blank_str + as_str + blank_str + \
+                mode_name[mode]+blank_str+from_str + blank_str+self.__table
+        else:
+            for it in self.__cols[:-1]:
+                # format_str += " columnName1, columnName2, ..."
+                format_str += blank_str + it + comma_str
+            # format_str += LIKE " COUNT(columnName) AS count_num " OR AVG OR SUM OR others
+            format_str += blank_str + mode_dict[mode] + left_str + \
+                self.__cols[-1] + right_str + blank_str + \
+                as_str + blank_str + mode_name[mode]
+            # format_str += "  FROM tableName"
+            format_str += blank_str + from_str + blank_str + \
+                self.__table
+            cols_output, format_str = self.naive_group(format_str, mode)
+        return cols_output, format_str
+
+    def naive_group(self, from_str, mode):
+        cols_output = []
+        format_str = from_str + blank_str + group_str
+        for it in self.__cols[:-2]:
+            cols_output.append(it)
+            format_str += blank_str + it + comma_str
+        format_str += blank_str + self.__cols[-2]
+        cols_output.append(mode_dict[mode])
+        return cols_output, format_str
+
+if __name__ == "__main__":
+    #run test for single module
+    a = single_module(["regionId","gender","user"])
+    print(a.naive_aggregation(1)[1])
