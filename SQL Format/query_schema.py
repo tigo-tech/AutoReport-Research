b9a13ebfe06443066be3eaf592248a448a6a4c96
elvis.zhang <1224522500@qq.com>
2019-05-21 00:09:12 +0800

    Add function for relation query

--- SQL Format/query_schema.py
+++ SQL Format/query_schema.py
@@ -1,4 +1,5 @@
 import pymysql
+import string
 
 
 # 列出表所有的列名
@@ -20,6 +21,26 @@ def list_table(cursor):
     cursor.execute("show tables")
     table_list = [tuple[0] for tuple in cursor.fetchall()]
     return table_list
+
+
+# 查询单表外键关系
+def list_fk(cursor, table_name):
+    cursor.execute("SELECT * FROM information_schema.INNODB_FOREIGN WHERE FOR_NAME LIKE %s OR REF_NAME LIKE %s"
+                   % ('%' + table_name, '%' + table_name))
+    table_relation_list = cursor.fetchall()
+    relation_dict = {}
+    table_relations = {}
+    for relation in table_relation_list:
+        relation_dict['fk_name'] = str(relation[0])[str(relation[0]).index("/")]
+        relation_dict['from_table'] = str(relation[1])[str(relation[1]).index("/")]
+        relation_dict['to_table'] = str(relation[2])[str(relation[2]).index("/")]
+        cursor.execute("SELECT * FROM information_schema.INNODB_FOREIGN_COLS WHERE ID = %s", relation_dict['fk_name'])
+        columns_list = cursor.fetchall
+        for columns in columns_list:
+            relation_dict['from_field'] = columns[1]
+            relation_dict['to_field'] = columns[2]
+        table_relations[relation_dict['fk_name']] = relation_dict
+    return table_relations
 
 
 # 初始化DB链接
@@ -44,3 +65,5 @@ if __name__ == '__main__':
         print(col_names)
         col_details = list_columns_detail(db_cursor, table)
         print(col_details)
+        # relations = list_fk(db_cursor, table)
+        # print(relations)
